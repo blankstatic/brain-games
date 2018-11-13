4725fca674fb11551861aca92169d7cbb34e8ad6
Aliendrop <drp-zzz@ya.ru>
2018-11-13 22:46:53 +0300

    add brain-gcd game template

---
+++ src/games/gcd.js
@@ -0,0 +1,8 @@
+import runGame from '..';
+
+const gameRules = 'Find the greatest common divisor of given numbers.\n';
+
+const gcdGame = () => {
+};
+
+export default () => runGame(gcdGame, gameRules);
