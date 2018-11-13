af2a7b1340564f096839e2e1595f97c9bc44346a
Aliendrop <drp-zzz@ya.ru>
2018-11-14 00:12:49 +0300

    add brain-gcd src

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Brain Games Set
 - brain-even
 - brain-calc
+- brain-gcd
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/d426db603c57aa7836a2/maintainability)](https://codeclimate.com/github/Aliendrop/project-lvl1-s376/maintainability)
 [![Build Status](https://travis-ci.com/Aliendrop/project-lvl1-s376.svg?branch=master)](https://travis-ci.com/Aliendrop/project-lvl1-s376)
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,8 +1,20 @@
+import getRandomInteger from '../utils';
 import runGame from '..';
 
 const gameRules = 'Find the greatest common divisor of given numbers.\n';
 
+const gameMaxValue = 100;
+const gameMinValue = 10;
+
+const findGcd = (a, b) => {
+  if (b === 0) return a;
+  return findGcd(b, a % b);
+};
+
 const gcdGame = () => {
+  const leftValue = getRandomInteger(gameMaxValue, gameMinValue);
+  const rigthValue = getRandomInteger(gameMaxValue, gameMinValue);
+  return [`${leftValue} ${rigthValue}`, String(findGcd(leftValue, rigthValue))];
 };
 
 export default () => runGame(gcdGame, gameRules);
