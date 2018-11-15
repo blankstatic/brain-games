3ecb7a07a26c8fbdb525f8e8cb298fa486ffbb8f
Aliendrop <drp-zzz@ya.ru>
2018-11-15 20:50:09 +0300

    add game menu

--- src/gameset.js
+++ src/gameset.js
@@ -1,8 +1,29 @@
-const gameset = ['brain-even', 'brain-calc', 'brain-gcd', 'brain-progression', 'brain-prime'];
-const gamesetMessage = `Сhoose one of the games:\n${gameset.join(', ')} and enjoy :)`;
+import readlineSync from 'readline-sync';
+import even from './games/even';
+import calc from './games/calc';
+import gcd from './games/gcd';
+import progression from './games/progression';
+import prime from './games/prime';
+
+const gameset = {
+  'brain-even': () => even(),
+  'brain-calc': () => calc(),
+  'brain-gcd': () => gcd(),
+  'brain-progression': () => progression(),
+  'brain-prime': () => prime(),
+};
 
 const showGames = () => {
-  console.log(`Welcome to the Brain Games!\n${gamesetMessage}\n`);
+  console.log('Available games set:');
+  const titles = Object.keys(gameset);
+  const games = Object.values(gameset);
+  const selectedGame = readlineSync.keyInSelect(titles, 'Сhoose one of them and enjoy :)');
+  if (selectedGame >= 0) {
+    console.log(`\nSelected <${titles[selectedGame]}> running...\n`);
+    games[selectedGame]();
+  } else {
+    console.log('Anyway, I\'ll see you tomorrow.');
+  }
 };
 
 export default showGames;
