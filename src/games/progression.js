7acc3295356f7a50a8e28092eda7704c40f66392
Aliendrop <drp-zzz@ya.ru>
2018-11-15 19:41:22 +0300

    refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,21 +4,26 @@ import runGame from '..';
 const gameRules = 'What number is missing in the progression?';
 
 const countProgressionItems = 10;
-const startProgressionMax = 10;
+const startProgressionMax = 100;
 const startProgressionMin = 1;
-const limitStepProgression = 10;
+const limitStepProgression = 5;
 const hiddenItemSymbols = '..';
+
+const createProgression = (progressionLength, startItem, stepValue) => {
+  const addItems = (progression, item, step, index = 0) => {
+    if (index !== progressionLength) {
+      progression.push(item);
+      addItems(progression, item + step, step, index + 1);
+    }
+  };
+  const progression = [];
+  addItems(progression, startItem, stepValue);
+  return progression;
+};
 
 const getAndHideItemProgression = (progression, index, hiddenSymbols) => {
   const hiddenItem = progression.splice(index, 1, hiddenSymbols);
   return String(hiddenItem);
-};
-
-const fillProgression = (progression, item, step, itemsLimit, currentIndex = 0) => {
-  if (currentIndex !== itemsLimit) {
-    progression.push(item);
-    fillProgression(progression, item + step, step, itemsLimit, currentIndex + 1);
-  }
 };
 
 const generateGameData = () => {
@@ -26,8 +31,7 @@ const generateGameData = () => {
   const progressionStep = getRandomInteger(limitStepProgression, 1);
   const hiddenItemIndex = getRandomInteger(countProgressionItems - 1);
 
-  const progression = [];
-  fillProgression(progression, startProgression, progressionStep, countProgressionItems);
+  const progression = createProgression(countProgressionItems, startProgression, progressionStep);
 
   const correctAnswer = getAndHideItemProgression(progression, hiddenItemIndex, hiddenItemSymbols);
   const question = progression.join(' ');
