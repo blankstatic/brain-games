d026af30b3bfdd5fb2967f242d9a287374478f1c
Aliendrop <drp-zzz@ya.ru>
2018-11-15 16:48:40 +0300

    add brain-prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,10 +1,22 @@
+import getRandomInteger from '../utils';
 import runGame from '..';
 
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const gameMaxValue = 30;
+const gameMinValue = 1;
+
+const isPrime = (n, i = 2) => {
+  if (n <= 2) return n === 2;
+  if (n % i === 0) return false;
+  if (i * i > n) return true;
+  return isPrime(n, i + 1);
+};
+
 const primeGame = () => {
-  const gameData = ['1', '1'];
-  return gameData;
+  const question = getRandomInteger(gameMaxValue, gameMinValue);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
 export default () => runGame(primeGame, gameRules);
