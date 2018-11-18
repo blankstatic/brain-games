1c57c7284856faa2f127e89ab36eab08faf010a8
Aliendrop <drp-zzz@ya.ru>
2018-11-18 19:01:58 +0300

    refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,38 +3,38 @@ import runGame from '..';
 
 const gameRules = 'What number is missing in the progression?';
 
-const progressionItemsCount = 10;
-const startProgressionMax = 100;
-const startProgressionMin = 1;
-const stepProgressionLimit = 5;
-const hiddenItemSymbol = '..';
+const itemsCount = 10;
+const startItemMax = 100;
+const startItemMin = 1;
+const stepLimit = 5;
+const hiddenSymbol = '..';
 
-const createProgression = (progressionLength, startItem, stepValue) => {
+const createProgression = (count, startItem, stepValue) => {
   const addItem = (item, step, limit, current = 0) => {
     if (current === limit) return item;
     return addItem(item + step, step, limit, current + 1);
   };
 
-  const progressionTemplate = Array(progressionLength).fill(0);
-  const progression = progressionTemplate.map((i, index) => addItem(startItem, stepValue, index));
+  const template = Array(count).fill(0);
+  const progression = template.map((i, index) => addItem(startItem, stepValue, index));
   return progression;
 };
 
-const progressionWithHiddenItem = (progression, index, symbols) => {
+const progressionHidden = (progression, index, symbols) => {
   const flow = progression.map((item, idx) => (index !== idx ? item : symbols));
   return flow;
 };
 
 const generateGameData = () => {
-  const startProgression = getRandomInteger(startProgressionMax, startProgressionMin);
-  const progressionStep = getRandomInteger(stepProgressionLimit, 1);
-  const hiddenItemIndex = getRandomInteger(progressionItemsCount - 1);
+  const start = getRandomInteger(startItemMax, startItemMin);
+  const step = getRandomInteger(stepLimit, 1);
+  const hiddenIndex = getRandomInteger(itemsCount - 1);
 
-  const progression = createProgression(progressionItemsCount, startProgression, progressionStep);
-  const desiredFlow = progressionWithHiddenItem(progression, hiddenItemIndex, hiddenItemSymbol);
+  const progression = createProgression(itemsCount, start, step);
+  const desiredFlow = progressionHidden(progression, hiddenIndex, hiddenSymbol);
 
   const question = desiredFlow.join(' ');
-  const correctAnswer = String(progression[hiddenItemIndex]);
+  const correctAnswer = String(progression[hiddenIndex]);
   return [question, correctAnswer];
 };
 
